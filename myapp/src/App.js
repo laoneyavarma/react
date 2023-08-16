1785c7c612d7768ca6af1f37b774ab5a9e92ad07
laoneyavarma <laoneyavarma@gmail.com>
2023-08-16 13:40:47 +0530

    name deleted

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -11,7 +11,7 @@ function App() {
       
       <Child  name="lava" age="27"/>
       <Child  name="suri" age="30"/>
-      <Child  name="jay" age="20"/>
+      
       
     <Parent name="lavanya" age="27"/>
     <Parent name="suresh" age="30"/>
